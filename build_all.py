d45b094fb3bec736a0c40eb806288636601caa0e
Marcos Scriven <marcosscriven@ubuntu32.(none)>
2013-03-07 19:49:02 +0000

    Added some examples

--- build_all.py
+++ build_all.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
+import os
 from tools.build import build_component
 
 INCLUDES_DIR = 'includes'
 LIBS_DIR = 'libs'
 
+if not os.path.exists(INCLUDES_DIR):
+    os.makedirs(INCLUDES_DIR)
+
+if not os.path.exists(LIBS_DIR):
+    os.makedirs(LIBS_DIR)
+
 COMPONENTS = [
-    #'boost',
-    #'gmp',
-    #'mpfr',
+    'boost',
+    'gmp',
+    'mpfr',
     'cgal',
 ]
 
--- components/boost/config.py
+++ components/boost/config.py
@@ -2,6 +2,7 @@ NAME = 'boost'
 VERSION = '1.53.0'
 DOWNLOADS = ['http://downloads.sourceforge.net/project/boost/boost/%s/boost_%s.tar.bz2' %
             tuple([VERSION, VERSION.replace('.', '_')])]
+SOURCE_DIR = 'boost-%s' % VERSION
 ARTIFACTS =  {
         'includes': [{
             'source':'boost_%s/boost' % VERSION.replace('.', '_'),
