c2803aa1fcc8dc739adb7012c379c224d2a3ed32
Marcos Scriven <marcosscriven@ubuntu32.(none)>
2013-03-07 20:07:40 +0000

    Revert to earlier version of Boost for now, as CGAL doesn't work with it

--- components/boost/config.py
+++ components/boost/config.py
@@ -1,5 +1,5 @@
 NAME = 'boost'
-VERSION = '1.53.0'
+VERSION = '1.49.0'
 DOWNLOADS = ['http://downloads.sourceforge.net/project/boost/boost/%s/boost_%s.tar.bz2' %
             tuple([VERSION, VERSION.replace('.', '_')])]
 SOURCE_DIR = 'boost-%s' % VERSION
--- examples/cgal/test.sh
+++ examples/cgal/test.sh
@@ -1,2 +1,2 @@
-em++ -U__SSE2_MATH__ -I ../../includes/cgal -I ../../includes/ ../../libs/libgmp.so ../../libs/libcgal.so AABB_custom_example.cpp
-nodejs a.out.js
+em++ -U__SSE2_MATH__ -I ../../includes/cgal -I ../../includes/ ../../libs/libgmp.so ../../libs/libcgal.so AABB_custom_example.cpp -o test.js
+nodejs test.js
